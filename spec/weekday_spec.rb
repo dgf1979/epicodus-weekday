baf9e6ea5b4084e25d527eedb6358e400d9d8703
Drew & Paul Ogawa <student@epicodus.com>
2015-04-21 10:07:08 -0700

    added test for input

--- spec/weekday_spec.rb
+++ spec/weekday_spec.rb
@@ -7,4 +7,12 @@ describe ('String£weekday') do
   it("return the day of the week week given a a date") do
     expect(("4/21/2015").weekday()).to(eq("Tuesday"))
   end
+
+  it('accepts a user inputted date to be inputted into the method') do
+    expect(("4/22/2015").weekday()).to(eq("Wednesday"))
+  end
+
+
+
+
 end
