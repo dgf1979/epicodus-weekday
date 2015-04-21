7375ad0bfe5c74ba55a59660edfc9978872c7323
Drew & Paul Ogawa <student@epicodus.com>
2015-04-21 10:25:12 -0700

    working commit - both tests passed

--- spec/weekday_spec.rb
+++ spec/weekday_spec.rb
@@ -4,9 +4,9 @@ require('pry')
 
 describe ('String£weekday') do
   #Test to see if today is tuesday
-#  it("return the day of the week week given a a date") do
-#    expect(("4/21/2015").weekday()).to(eq("Tuesday"))
-#  end
+  it("return the day of the week week given a a date") do
+    expect(weekday("4/21/2015")).to(eq("Tuesday"))
+  end
 
   it('accepts a user inputted date to be inputted into the method') do
     expect(weekday("4/22/2015")).to(eq("Wednesday"))
