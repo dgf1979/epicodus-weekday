348068065e50a92c69524cef003acfe627966745
Drew & Paul Ogawa <student@epicodus.com>
2015-04-21 10:30:51 -0700

    reviewed working code

--- spec/weekday_spec.rb
+++ spec/weekday_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('weekday')
 require('pry')
 
-describe ('String£weekday') do
+describe ('String#weekday') do
   #Test to see if today is tuesday
   it("return the day of the week week given a a date") do
     expect(weekday("4/21/2015")).to(eq("Tuesday"))
